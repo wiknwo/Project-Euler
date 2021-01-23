5a42b5201594fbba8957597fa5838e75025083ae
wiknwo <spinningthewww@outlook.com>
2021-01-23 09:14:20 +0000

    Changed the comment

--- problem6.cpp
+++ problem6.cpp
@@ -1,11 +1,15 @@
 /**
  * William Ikenna-Nwosu (wiknwo)
  *
- * 2520 is the smallest number that can be divided by each of the 
- * numbers from 1 to 10 without any remainder.
+ * The sum of the squares of the first ten natural numbers is 385
  *
- * What is the smallest positive number that is evenly divisible 
- * by all of the numbers from 1 to 20?
+ * The square of the sum of the first ten natural numbers is 3025
+ * 
+ * Hence the difference between the sum of the squares of the 
+ * first ten natural numbers and the square of the sum is 2640
+ *
+ * Find the difference between the sum of the squares of the 
+ * first one hundred natural numbers and the square of the sum.
  **/
 #include <iostream>
 using namespace std;
