52633923354036da38c7104451e3949703b23137
wiknwo <spinningthewww@outlook.com>
2021-02-02 17:33:57 +0000

    Problem Solved

---
+++ problem15.py
@@ -0,0 +1,15 @@
+'''Problem 15
+
+    William Ikenna-Nwosu (wiknwo)
+
+    Starting in the top left corner of a 2×2 grid, and only 
+    being able to move to the right and down, there are 
+    exactly 6 routes to the bottom right corner.
+
+    How many such routes are there through a 20×20 grid?
+'''
+from math import comb
+def main():
+    print(comb(40,20))
+if __name__ == '__main__':
+    main()
